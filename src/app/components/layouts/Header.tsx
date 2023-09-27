4fbb7b1085dc8d4224d27c4022f9c6a1b183576d
SerraAugusto <augusto.luis.serra@gmail.com>
2023-09-27 16:36:30 -0300

    fix cv test

--- src/app/components/layouts/Header.tsx
+++ src/app/components/layouts/Header.tsx
@@ -128,7 +128,7 @@ export default function Header() {
     <Container>
       <LeftSide>
         <NameLogo href='#Home'>Augusto</NameLogo>
-        <ResumeButton filename='./Augusto-Serra-CV-Esp.pdf' buttonText='C.V.' ></ResumeButton> 
+        <ResumeButton filename='Augusto-Serra-CV-Esp.pdf' buttonText='C.V.' ></ResumeButton> 
         {/*<SunIcon></SunIcon><ThemeSwitcherButton></ThemeSwitcherButton><MoonIcon></MoonIcon> */}
         {/*<ButtonStyle  href="https://drive.google.com/file/d/1ObK1j0t1klr4vX_FcslESXiNsWWWidDx/view?usp=drive_link" target="_blank">C.V</ButtonStyle>*/}
       </LeftSide>
